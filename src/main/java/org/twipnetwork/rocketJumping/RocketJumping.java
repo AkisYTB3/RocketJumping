582fbafa53f6a0d27058357773f5d6a273257da2
Richard Mikloš <akisytbsteamy@gmail.com>
2024-07-25 14:11:44 +0200

    init

---
+++ src/main/java/org/twipnetwork/rocketJumping/RocketJumping.java
@@ -0,0 +1,67 @@
+package org.twipnetwork.rocketJumping;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
+import org.bukkit.entity.Firework;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
+
+public final class RocketJumping extends JavaPlugin implements Listener {
+
+    private double boostPower;
+    private boolean damageShooter;
+    private boolean damageOthers;
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+        FileConfiguration config = getConfig();
+        boostPower = config.getDouble("boostPower", 4.0);
+        damageShooter = config.getBoolean("damageShooter", false);
+        damageOthers = config.getBoolean("damageOthers", true);
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+    @EventHandler
+    public void onProjectileHit(ProjectileHitEvent event) {
+        Projectile projectile = event.getEntity();
+
+        if (projectile instanceof Firework) {
+            Firework firework = (Firework) projectile;
+            Entity shooter = (Entity) firework.getShooter();
+
+            for (Entity entity : firework.getNearbyEntities(5, 5, 5)) {
+                if (entity instanceof Player) {
+                    Player player = (Player) entity;
+                    Vector boostDirection = player.getLocation().toVector().subtract(firework.getLocation().toVector()).normalize();
+                    player.setVelocity(player.getVelocity().add(boostDirection.multiply(boostPower)));
+
+                    if (!damageShooter && player.equals(shooter)) {
+                        player.setNoDamageTicks(1);
+                    }
+                } else {
+                    Vector boostDirection = entity.getLocation().toVector().subtract(firework.getLocation().toVector()).normalize();
+                    entity.setVelocity(entity.getVelocity().add(boostDirection.multiply(boostPower)));
+                }
+            }
+
+            if (!damageOthers) {
+                for (Entity entity : firework.getNearbyEntities(5, 5, 5)) {
+                    if (!(entity instanceof Player && entity.equals(shooter))) {
+                        ((Player) entity).setNoDamageTicks(1);
+                    }
+                }
+            }
+        }
+    }
+}
